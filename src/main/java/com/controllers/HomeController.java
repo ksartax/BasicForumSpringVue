2c7f878ddf9e191f5f744f12e32ef63589115b97
Damian Stępniak <damianos-11@o2.pl>
2017-11-16 06:16:51 +0100

    Zamiana na tag security + logout

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -1,7 +1,14 @@
 package com.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping(path = "/")
 @Controller
@@ -11,5 +18,15 @@ public class HomeController {
     @RequestMapping(path = "/")
     public String index() {
         return HomeController.DEFAULT_TEMPLATE + "index";
+    }
+
+    @RequestMapping(path = "/logout")
+    public ModelAndView logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return new ModelAndView("redirect:/");
     }
 }
